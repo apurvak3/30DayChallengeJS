82e4e81015b551862d85b51776c7b068263f8de7
Apurva <117301920+apurvak3@users.noreply.github.com>
2024-07-13 09:32:11 +0530

    Add files via upload
    Day 1- of 30 days of challenge of JavaScript

---
+++ day1.js
@@ -0,0 +1,38 @@
+//Activity 1
+//Declare a variable using var,assign it a number and log the value to the console.
+var number = 23;
+console.log(number);
+
+//Declare a string using let,assign it a number and log the value to the console.
+let str = '23';
+console.log(str);
+
+//Activity-2
+//Declare a variable using const,assign it a boolean value and log the value to the console.
+const bool = false;
+console.log(bool);
+
+//Activity-3 Data Types
+//Create variable of different DataTypes (number,strings,boolean,object,array) and log each variables type using the type of operator
+const myObject = {name :'apurva', roll:'22001013022'};
+const myObject1 = ['apurva', 'sanjana', 'Ankita'];
+console.log(typeof number);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof myObject);
+console.log(typeof myObject1);
+
+//Activity -4 Reassigning variable
+//declare a variable using let,assign it an initial value, reassign it to new value and log both value to console.
+let mycity = 'Delhi';
+ mycity = 'bombay';
+console.log(mycity);
+
+//Activity-5 Understanding const
+//Try reassigning a variable declared with const and observe the error
+let myName ='Apurva';
+let myName ='khushi';
+console.log(myName); 
+
+
+
