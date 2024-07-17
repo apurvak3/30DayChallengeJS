7fab9f3200f06af90a885ab46553b159507685bf
Apurva <117301920+apurvak3@users.noreply.github.com>
2024-07-17 09:35:35 +0530

    Add files via upload

---
+++ day4/Activity1.js
@@ -0,0 +1,13 @@
+//Activity 1;
+/**
+ * Task 1:Write a program to print numbers from 1 to 10 using for loop.
+ */
+
+for (let i=1 ; i<=10 ; i++){
+    console.log(i);
+}
+
+//Task 2:write a program to write a multiplication table of 2 using a for loop
+for(let i =1;i<= 10 ; i++){
+    console.log(`2 x ${i} = ${2 * i}`);
+}
