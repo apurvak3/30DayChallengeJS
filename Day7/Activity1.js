f8bb9d6ed8068a63889b3eb13aa64b64ade8c1a7
Apurva <117301920+apurvak3@users.noreply.github.com>
2024-07-21 22:40:52 +0530

    Add files via upload

---
+++ Day7/Activity1.js
@@ -0,0 +1,15 @@
+//day7
+/**
+ * Activity1.
+ * task1:Create an object representong a book with properties like title, author and year and log the object to the console.
+ * task2:Acess and log the the title and author properties of the book object.
+ */
+let book = {
+    title: 'The Revenge of Mine',
+    Author : 'Apurva Kanth',
+    year : '2029',
+}
+
+console.log(book);
+console.log(book.Author)
+console.log(book.title);
---
+++ Day7/Activity3.js
@@ -0,0 +1,7 @@
+//Nested object
+/**
+ * task5: create a nested object representing a library with properties like name and book (an array of book oject ), and 
+ * log the library object to the console.
+ * task6:Access and log tha name of the library and the titles of all the books in the library.
+ * 
+ */
