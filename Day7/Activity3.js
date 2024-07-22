c72b6e2505bc96cf8e45128e5fa159ae86ffc6c9
Apurva <117301920+apurvak3@users.noreply.github.com>
2024-07-22 07:01:02 +0530

    Update Activity3.js

--- Day7/Activity3.js
+++ Day7/Activity3.js
@@ -5,3 +5,32 @@
  * task6:Access and log tha name of the library and the titles of all the books in the library.
  * 
  */
+let book1 = {
+    author :"Apurva",
+    name:  "I love Myself",
+    year : 2006,
+getdetails : function (){
+ return `author :  ${this.author}, name: ${this.name} , year: ${this.year}`
+ },
+ updateYear :function(newYear){
+    this.year = newYear 
+ }  
+}
+console.log(book1.getdetails());
+console.log(book1.updateYear(2027));
+let book2 = {
+    author: "sanjana",
+    name : "lets break the rule",
+    year : 2007,
+    getdetails2 : function(){
+        return `author: ${this.author} , name: ${this.name} , year: ${this.year}`
+    }
+}
+console.log(book2.getdetails2());
+ let library ={
+    book : [book1 , book2],
+    getDetails3: function(){
+        return  this.books.map(book => book.getdetails ? book.getdetails() : book.getdetails2()).join("; ");
+    }
+ }
+ console.log(library.getDetails3());
