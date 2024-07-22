d45f224ca7a38b72a1b797a1e847088445728db3
Apurva <117301920+apurvak3@users.noreply.github.com>
2024-07-22 06:59:26 +0530

    Update Activity5.js

---
+++ Day7/Activity5.js
@@ -0,0 +1,19 @@
+//Activity5
+/**
+ * task:8 use a for...in loop to iterate over the properties of book object and log each property and its value.
+ * task:9 use object.key and object.values method to log all the keys and values of  the book object
+ */
+let book3 = {
+    author: 'Apurva kanth',
+    name : 'The life At seria',
+    year : 2021,
+    getDetail : function(){
+    return `author :${this.author} name of the book: ${this.name} year: ${this.year}`;
+    },
+    updatedYear : function(newYear){
+     this.year = newYear;
+    }
+}
+console.log(book3.getDetail());
+console.log(book3.updatedYear(2023));
+console.log(book3.getDetail());
