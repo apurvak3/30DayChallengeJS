976f327c491bc5ee3b5a6a7764b95073e4e10ac2
Apurva <117301920+apurvak3@users.noreply.github.com>
2024-07-25 01:15:29 +0530

    Add files via upload

---
+++ day9/Activity3.js
@@ -0,0 +1,10 @@
+//Removing Elements
+/**
+ * task5:select an html element and remove it from the dom
+ * task6:Remove the last element of the specific Html element
+ */
+ // Select the element to be removed
+ var listItem = document.getElementById("item2");
+        
+ // Remove the element from the DOM
+ listItem.remove();
