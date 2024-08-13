690a0961e53baa0269d29fb507f6217a7f068267
Apurva <117301920+apurvak3@users.noreply.github.com>
2024-08-13 06:52:47 +0530

    Update task4.js

--- day11/Activity3/task4.js
+++ day11/Activity3/task4.js
@@ -1,1 +1,19 @@
 //write a async function that waits for a promise to resolve and then logs a resolved value. 
+
+function fetchData(){
+    return new Promise((resolve) =>{
+        setTimeout(() => {
+        resolve("Data fetched from server");
+        },2000)
+    })
+}
+async function logResolvedValue() {
+    try{
+        const Data = await fetchData();
+        console.log("Resolved value:", Data);
+
+    }catch(error){
+        console.log("Hey error occured", error)
+    }
+    
+}
